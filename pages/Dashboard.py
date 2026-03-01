e499a05cd77777322d85dd235649f878990b47c4
Khanolkar sujal <khanolkarsujal@gmail.com>
2026-03-01 14:07:21 +0530

    fix survive percentage problem

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -7,7 +7,7 @@ st.title(" Month Survival Dashboard ")
 
 
 if "Daily Safe Spend" in st.session_state:
-    st.write('Daily Safe Spend: ',st.session_state["Daily Safe Spend"] )
+    st.metric("Daily Safe Spend", f"₹{st.session_state['Daily Safe Spend']}")
 
 else: 
     st.write("Daily Safe Spend is not calulated yet")
@@ -15,4 +15,18 @@ else:
 
 st.write(" Survival Meter Bar" )
 
-         
+
+if "Available Money" in st.session_state and "Daily Safe Spend" in st.session_state:
+
+    available = st.session_state["Available Money"]
+    daily_safe = st.session_state["Daily Safe Spend"]
+    days_remaining = st.session_state["Days Remaining"]
+    current_daily_spend = st.session_state["Current Daily Spend"]
+
+
+    if current_daily_spend > 0 and days_remaining > 0:
+        survival_days = available / current_daily_spend
+        survival_percentage = round((survival_days / days_remaining) * 100, 2)
+    
+    st.progress(min(int(survival_percentage), 100))
+    st.write(" Survival percentage", survival_percentage,"%")
--- pages/Survive_Mode.py
+++ pages/Survive_Mode.py
@@ -8,26 +8,38 @@ last_day = calendar.monthrange(today.year, today.month)[1]
 days_remaining = last_day - today.day 
 
 
-def survive_mode(remaining_money , days_remaining):
-
-    return remaining_money /days_remaining
+def survive_mode(Available, days_remaining):
+    if days_remaining > 0:
+        return round(Available / days_remaining, 2)
+    else:
+        return 0        
 
 
 st.title("Setup Your Month ")
 
-total_money = st.number_input("Total Money: ₹____ ", min_value=0)
+total_money = st.number_input("Total Money: ", min_value=0)
 
-fixed_expenses = st.number_input("Fixed Expenses: ₹____", min_value=0)
+fixed_expenses = st.number_input("Fixed Expenses: ", min_value=0)
+
+current_daily_spend = st.number_input("Current Daily Spending", min_value=0) 
 
 st.write('Days Remaining:  ',days_remaining)
 
 
 if st.button("Calculate Survival"): 
 
-    calculate_survival = survive_mode(total_money-fixed_expenses, days_remaining)
+    Available = total_money - fixed_expenses
 
-    st.session_state["Daily Safe Spend"]  = calculate_survival
+    if Available < 0:
+        st.error("Fixed expenses cannot be greater than total money.")
+    else:
+        calculate_survival = survive_mode(Available, days_remaining)
+
+        st.session_state["Daily Safe Spend"] = calculate_survival
+        st.session_state["Available Money"] = Available
+        st.session_state["Days Remaining"] = days_remaining
+        st.session_state["Current Daily Spend"] = current_daily_spend
 
 
-    st.write('Daily Safe Spend: ',calculate_survival )
+        st.write('You can spend ₹', calculate_survival, 'per day to survive.')
 
