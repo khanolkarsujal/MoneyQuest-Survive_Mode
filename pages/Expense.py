717123336655811cb0d9fae0c66b4a2fe2cbce47
Khanolkar sujal <khanolkarsujal@gmail.com>
2026-03-01 16:46:33 +0530

    completed expense

---
+++ pages/Expense.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import os
+
+file_path = "data.csv"
+
+st.title("Add Expense")
+
+
+amount = st.number_input("Amount: ", min_value=0, step=1)
+
+
+options = [ "Food", "Transport", "Entertainment", "Utilities", "Other Expenses"]
+category = st.selectbox("Category (Optional)", options)
+
+
+if st.button("Save"):
+
+    if os.path.exists(file_path):
+        df = pd.read_csv(file_path)
+    else:
+        df = pd.DataFrame(columns=["Amount", "Category"])
+
+
+    new_row = {"Amount": amount, "Category": category}
+    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+
+    
+    df.to_csv(file_path, index=False)
+
+    st.success("Saved successfully!")
