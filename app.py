6595bb1e4654bf434d17923261b110c7374602ff
Khanolkar sujal <khanolkarsujal@gmail.com>
2026-03-01 16:59:43 +0530

    completed expense

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import streamlit as st
-import pandas as pd 
 
-
-st.title("MoneyQuest")
-
-st.subheader("Will your money survive?")
-
+st.title("💰 MoneyQuest")
+st.subheader("Will your money survive this month?")
 
 if st.button("Start Survival Mode"):
-    st.switch_page("pages/Survive_Mode.py")
+    st.switch_page("Survive_Mode")
 
-
-
+if st.button("+ Add Expense"):
+    st.switch_page("Expense")
